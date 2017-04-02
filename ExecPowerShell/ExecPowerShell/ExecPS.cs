99234f0774a4b4410213f2ba81df7a3f4cf3d385
kento miko <miko.kento@gmail.com>
2017-04-02 16:03:58 +0900

    PowerShell側のパラメータ指定に対応

--- ExecPowerShell/ExecPowerShell/ExecPS.cs
+++ ExecPowerShell/ExecPowerShell/ExecPS.cs
@@ -4,6 +4,7 @@
 namespace ExecPowerShell
 {
     using System;
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Management.Automation;
     using System.Management.Automation.Runspaces;
@@ -13,7 +14,32 @@ namespace ExecPowerShell
     /// </summary>
     public class ExecPS
     {
+        /// <summary>
+        /// PowerShell引数のタイプ
+        /// </summary>
+        public enum paramType{
+            ARGMENT,PARAMETER,
+        }
+
+        /// <summary>
+        /// パラメータ保持用
+        /// </summary>
+        public struct ParamStruct
+        {
+            public paramType paramType;
+            public string paramName;
+            public string paramValue;
+        }
+
+        /// <summary>
+        /// 引数用
+        /// </summary>
         private Collection<PSObject> adapts;
+
+        /// <summary>
+        /// パラメータ保持用リスト
+        /// </summary>
+        private List<ParamStruct> paramList;
 
         /// <summary>
         /// コンストラクタ
@@ -21,6 +47,35 @@ namespace ExecPowerShell
         public ExecPS()
         {
             this.adapts = null;
+            paramList = new List<ParamStruct>();
+        }
+
+        /// <summary>
+        /// パラメータを設定
+        /// </summary>
+        /// <param name="paramList">パラメータ保持用リスト</param>
+        /// <param name="pt">パラメータのタイプを指定する(Argment, Parameter)</param>
+        /// <param name="paramName"></param>
+        /// <param name="paramValue"></param>
+        public static void AddParam(ref List<ParamStruct> paramList, paramType pt, string paramName, string paramValue = null)
+        {
+            try
+            {
+                ParamStruct pStruct = new ParamStruct();
+                // パラメータタイプを設定
+                pStruct.paramType = pt;
+                // パラメータ名を指定
+                pStruct.paramName = paramName;
+                // パラメータにセットする値を設定
+                pStruct.paramValue = paramValue;
+
+                // リストに追加
+                paramList.Add(pStruct);
+            }
+            catch (Exception exp)
+            {
+                throw exp;
+            }
         }
 
         /// <summary>
@@ -42,6 +97,70 @@ namespace ExecPowerShell
                         PSCommand psCmd = new PSCommand();
 
                         psCmd.AddCommand(psScriptName);
+
+                        ps.Commands = psCmd;
+                        ps.Runspace = rs;
+
+                        // スクリプトを実行する
+                        adapts = ps.Invoke();
+                    }
+                }
+
+                return adapts;
+            }
+            catch (Exception exp)
+            {
+                throw exp;
+            }
+        }
+ 
+        /// <summary>
+        /// PSスクリプト実行（引数設定済み）
+        /// </summary>
+        /// <param name="psScriptName"></param>
+        /// <returns></returns>
+        public Collection<PSObject> ExecPowerShell(string psScriptName, List<ParamStruct> paramList)
+        {
+            // パラメータがセットされていない場合は終了
+            if (paramList == null)
+            {
+                return null;
+            }
+
+            try
+            {
+                using (Runspace rs = RunspaceFactory.CreateRunspace())
+                {
+                    // Runspace をオープンする
+                    rs.Open();
+
+                    using (PowerShell ps = PowerShell.Create())
+                    {
+                        PSCommand psCmd = new PSCommand();
+
+                        psCmd.AddCommand(psScriptName);
+
+                        foreach (ParamStruct item in paramList)
+                        {
+                            switch (item.paramType)
+                            {
+                                case paramType.ARGMENT:
+                                    psCmd.AddArgument(item.paramName);
+                                    break;
+                                case paramType.PARAMETER:
+                                    if (item.paramValue == null)
+                                    {
+                                        psCmd.AddParameter(item.paramName);
+                                    }
+                                    else
+                                    {
+                                        psCmd.AddParameter(item.paramName, item.paramValue);
+                                    }
+                                    break;
+                                default:
+                                    return null;
+                            }
+                        }
 
                         ps.Commands = psCmd;
                         ps.Runspace = rs;
